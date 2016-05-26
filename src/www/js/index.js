f39d0dc4bb7e1c65a71571460633bab2ec19f030
Eric W. Greene <eric@ericwgreene.com>
2016-05-26 15:12:23 -0400

    add react input demo

--- src/www/js/index.js
+++ src/www/js/index.js
@@ -5,13 +5,36 @@ const colors = ['red','gold','green','white','saffron','blue'];
 
 class HelloWorld extends React.Component {
 
+	static get propTypes() {
+		return {
+			title: React.PropTypes.string.isRequired,
+			items: React.PropTypes.array.isRequired
+		};
+	}
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			newItem: ''
+		};
+
+		this.onChange = this.onChange.bind(this);
+	}
+
+	onChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	}
+
 	render() {
 		//return React.createElement('h1', null, 'Hello World!');
 		//return <h1>Hello World!</h1>;
 
 		const listItems = [];
 
-		this.props.colors.forEach(function(item) {
+		this.props.items.forEach(function(item) {
 			listItems.push(<li key={item}>{item}</li>);
 		});
 
@@ -20,12 +43,30 @@ class HelloWorld extends React.Component {
 			<ul>
 				{listItems}
 			</ul>
+			<form>
+				<label>
+					New Item:
+					<input type='text' name='newItem' value={this.state.newItem} onChange={this.onChange} />
+				</label>
+			</form>
 		</div>;
 	}
 
 }
 
+const widgets = [
+	{ id: 1, name: 'Widget 1', description: 'Widget 1 Desc', color: 'red', size: 'large', quantity: 1},
+	{ id: 2, name: 'Widget 2', description: 'Widget 2 Desc', color: 'blue', size: 'small', quantity: 2},
+	{ id: 3, name: 'Widget 3', description: 'Widget 3 Desc', color: 'black', size: 'medium', quantity: 3}
+];
+
 class WidgetTable extends React.Component {
+
+	static get propTypes() {
+		return {
+			widgets: React.PropTypes.array
+		};
+	}
 
 	render() {
 		return <table className='table table-striped'>
@@ -40,10 +81,20 @@ class WidgetTable extends React.Component {
 				</tr>
 			</thead>
 			<tbody>
+				{this.props.widgets.map(widget =>
+					<tr key={widget.id}>
+						<td>{widget.id}</td>
+						<td>{widget.name}</td>
+						<td>{widget.description}</td>
+						<td>{widget.color}</td>
+						<td>{widget.size}</td>
+						<td>{widget.quantity}</td>
+					</tr>)}
 			</tbody>
 		</table>;
 	}
 
 }
 
-ReactDOM.render(<HelloWorld colors={colors} title='ADP Rocks!' />, document.querySelector('main'));
+ReactDOM.render(<HelloWorld items={colors} title='ADP Rocks!' />, document.querySelector('main'));
+//ReactDOM.render(<WidgetTable widgets={widgets} />, document.querySelector('main'));
